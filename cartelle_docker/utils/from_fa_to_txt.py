5de6333eca7ad7b97bd7902b761e386090133016
Mirkocoggi <mirko1998coggi@gmail.com>
2023-06-22 14:10:47 +0200

    New data and modified GA dockerfile

--- cartelle_docker/utils/from_fa_to_txt.py
+++ cartelle_docker/utils/from_fa_to_txt.py
@@ -1,6 +1,6 @@
 
-input_file = 'shared_input/input_data/SARS-CoV2.fa'
-output_file = 'shared_input/input_data/SARS-CoV2.txt'
+input_file = 'shared_input/input_data/reads_umane/C4-96.fa'
+output_file = 'shared_input/input_data/reads_umane/C4-96.txt'
 with open(input_file, 'r') as file:
         with open(output_file, 'w') as output:
             for line in file:
