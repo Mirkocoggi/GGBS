628d53ebc3d4c16eee03adb485609fdbebf414da
Mirkocoggi <mirko1998coggi@gmail.com>
2023-07-12 16:55:20 +0000

    hisat2 and vg parser

--- cartelle_docker/utils/vg_gaf_parser.py
+++ cartelle_docker/utils/vg_gaf_parser.py
@@ -1,14 +1,32 @@
 import csv
 
-with open('output_gen/out_vg.gaf', 'r') as f_in:
+import re
+
+def sum_numbers_after_colon(cigar_cs):
+    numbers = re.findall(r':(\d+)', cigar_cs)
+    total = sum(map(int, numbers))
+    return total
+global_score = 0 
+i = 0
+with open('output_gen/out/a_vg_n.gaf', 'r') as f_in:
     with open('results/vg_gaf.csv', 'w') as f_out:
-        writer = csv.writer(f_out)
-        writer.writerow(['read_id', 'read_length', 'offset', 'alignment_length', 'strand', 'query_name', 'query_length', 'query_start', 'query_end', 'target_start', 'target_end', 'quality_score', 'cigar_cs'])
+        
         for line in f_in:
+            i=i+1
+            print(i)
             if line.startswith('#'):
                 continue
             values = line.strip().split('\t')
-            cs_value = values[-2].split('cs:Z:')[1]
-            csv_values = values[:12]
-            csv_values.append(cs_value)
-            writer.writerow(csv_values)
+            if values[2] == "*":
+                continue
+            value = values[13]
+            cs_values = values[13].split('cs:Z:')
+            cs_value = cs_values[1]
+            total_score = int(values[1]) - sum_numbers_after_colon(cs_value)
+            global_score = global_score + total_score
+            # csv_values = values[:12]
+            # csv_values.append(cs_value)
+            # writer.writerow(csv_values)
+
+
+print("total score: ", global_score) 
